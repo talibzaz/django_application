9246f05c4095d1ed742370c7da5dbd1575497153
Talib Zaz <musist222@gmail.com>
2018-12-22 22:24:29 +0530

    minor fixes

--- django_application/urls.py
+++ django_application/urls.py
@@ -3,11 +3,13 @@ from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from drogo import views as drogo_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('drogo/', include('drogo.urls')),
     path('accounts/', include('accounts.urls')),
+    path('', drogo_views.article_list, name='home')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
